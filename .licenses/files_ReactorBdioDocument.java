53aea54c7533faf92f6f3d580e7b42f9e2e155a4
GitHub Action <action@github.com>
2023-08-07 19:27:46 +0000

    Adding license findings via GitHub Actions

---
+++ .licenses/files_ReactorBdioDocument.java
@@ -0,0 +1,10 @@
+{
+  "logRef": "hub-scan_ERqaidd8",
+  "errorMessage": "Licensing issue: Unable to manage operation because feature access is prohibited for the following features: Generative AI Compliance Service.  Contact your system administrator.",
+  "arguments": {
+    "errorMessage": "Unable to manage operation because feature access is prohibited for the following features: Generative AI Compliance Service.  Contact your system administrator."
+  },
+  "errorCode": "{core.rest.payment.required}",
+  "errors": [],
+  "links": []
+}
